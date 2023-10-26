4712fa7f5d8851ee6998f05d650211590ca78ffd
ottagit <ottaosawa@gmail.com>
2023-10-26 21:09:12 +0300

    output all created IAM users

--- landing-zone/iam-user/outputs.tf
+++ landing-zone/iam-user/outputs.tf
@@ -2,3 +2,8 @@ output "user_arn" {
   value = aws_iam_user.example.arn
   description = "The ARN of the created IAM user"
 }
+
+output "all_users" {
+  value = aws_iam_user.example
+  description = "The created IAM users"
+}
