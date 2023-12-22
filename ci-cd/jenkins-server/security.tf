08f12a643848d4e2585e46f5d6abed1a2a1fe86b
ottagit <ottaosawa@gmail.com>
2023-12-22 10:55:11 +0300

    listen on port 8080 for ingress

--- ci-cd/jenkins-server/security.tf
+++ ci-cd/jenkins-server/security.tf
@@ -9,6 +9,14 @@ resource "aws_security_group" "outbound-inbound-all" {
     to_port = 22
     protocol = "tcp"
   }
+  
+  ingress {
+    cidr_blocks = ["0.0.0.0/0"]
+    from_port = 8080
+    to_port = 8080
+    protocol = "tcp"
+  }
+  
   # Allow all outbound traffic
   egress {
     from_port = 0
