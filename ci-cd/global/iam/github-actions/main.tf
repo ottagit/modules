465149d0d6aaa03a8779930c48c60ffee68a88d5
ottagit <ottaosawa@gmail.com>
2024-01-24 13:43:48 +0300

    create role for GitHub OIDC

--- ci-cd/global/iam/github-actions/main.tf
+++ ci-cd/global/iam/github-actions/main.tf
@@ -1,8 +1,8 @@
 # Create an IAM OIDC identity provider that trusts GitHub
 resource "aws_iam_openid_connect_provider" "github_actions" {
-  url = "https://token.actions.githubusercontent.com"
-  client_id_list = ["sts.amazonaws.com"]
-  thumbprint_list = [ data.tls_certificate.github.certificates[0].sha1_fingerprint ]
+  url             = "https://token.actions.githubusercontent.com"
+  client_id_list  = ["sts.amazonaws.com"]
+  thumbprint_list = [data.tls_certificate.github.certificates[0].sha1_fingerprint]
 }
 
 # Fetch GitHub's OIDC thumbprint
@@ -14,32 +14,74 @@ data "tls_certificate" "github" {
 # role via federated authentication
 data "aws_iam_policy_document" "assume_role_policy" {
   statement {
-    actions = [ "sts:AssumeRoleWithWebIdentity" ]
-    effect = "Allow"
+    actions = ["sts:AssumeRoleWithWebIdentity"]
+    effect  = "Allow"
     principals {
-      identifiers = [ aws_iam_openid_connect_provider.github_actions.arn ]
-      type = "Federated"
+      identifiers = [aws_iam_openid_connect_provider.github_actions.arn]
+      type        = "Federated"
     }
 
     condition {
-      test = "StringEquals"
+      test     = "StringEquals"
       variable = "token.actions.githubusercontent.com:sub"
       # The repos and branches defined in var.allowed_repos_branches
       # will be able to assume this role
-      values = [ 
+      values = [
         for a in var.allowed_repos_branches :
 
         "repo:${a["org"]}/${a["repo"]}:ref:refs/heads/${a["branch"]}"
-       ]
+      ]
     }
   }
 }
 
-module "ci_server_role" {
-  source = "github.com/ottagit/modules//ci-cd/global/iam/roles/ci-server?ref=v0.1.3"
+# Create an IAM role and pass it the JSON from the assume_role
+# policy document to use as the assume role policy
+resource "aws_iam_role" "github_actions" {
+  name_prefix        = var.name
+  assume_role_policy = data.aws_iam_policy_document.assume_role.json
+}
 
-  name = "github-action-role"
-  dynamo_db_table   = "terraone-locks"
-  s3_bucket_name    = "batoto-bitange"
-  path_to_key       = "global/iam/github-actions/githuboidc.tfstate"
+# Define policies to attach to the CI server instance IAM role
+data "aws_iam_policy_document" "github_actions_admin_permissions" {
+  statement {
+    effect    = "Allow"
+    actions   = ["ec2:*", ]
+    resources = ["*"]
+  }
+
+  statement {
+    sid = "S3Backend"
+
+    effect = "Allow"
+    actions = [
+      "s3:ListBucket",
+      "s3:GetObject",
+      "s3:PutObject",
+      "s3:DeleteObject",
+    ]
+
+    resources = [
+      "arn:aws:s3:::${var.s3_bucket_name}",
+      "arn:aws:s3:::${var.s3_bucket_name}/${var.path_to_key}",
+    ]
+  }
+  statement {
+    sid = "StateLockTable"
+
+    effect = "Allow"
+    actions = [
+      "dynamodb:DescribeTable",
+      "dynamodb:GetItem*",
+      "dynamodb:DeleteItem*",
+      "dynamodb:PutItem",
+    ]
+    resources = ["arn:aws:dynamodb:*:*:table/${var.dynamo_db_table}"]
+  }
+}
+
+# Attach resource admin policies to CI server IAM role
+resource "aws_iam_role_policy" "github_actions" {
+  role   = aws_iam_role.github_actions.id
+  policy = data.aws_iam_policy_document.github_actions_admin_permissions.json
 }
--- ci-cd/global/iam/github-actions/variables.tf
+++ ci-cd/global/iam/github-actions/variables.tf
@@ -1,8 +1,28 @@
 variable "allowed_repos_branches" {
   description = "GitHub repos/branches allowed to assume the IAM role."
   type = list(object({
-    org = string
-    repo = string
+    org    = string
+    repo   = string
     branch = string
   }))
+}
+
+variable "name" {
+  description = "The name prefix of the CI instance IAM role"
+  type        = string
+}
+
+variable "dynamo_db_table" {
+  description = "The Dynamo DB table that stores the state lock file"
+  type        = string
+}
+
+variable "s3_bucket_name" {
+  description = "The name of the S3 backend bucket"
+  type        = string
+}
+
+variable "path_to_key" {
+  description = "The path to the Terraform state key"
+  type        = string
 }
