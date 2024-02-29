09e2c7627d77ccb04c8bb35a831f425c3ece43c7
ottagit <ottaosawa@gmail.com>
2024-02-29 09:02:07 +0300

    include provider and version info in module

--- data-stores/mysql/main.tf
+++ data-stores/mysql/main.tf
@@ -1,3 +1,22 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+  alias = "primary"
+}
+
+provider "aws" {
+  region = "ap-south-1"
+  alias = "replica"
+}
+
 resource "aws_db_instance" "example" {
   identifier_prefix = "example-db"
   allocated_storage = 10
