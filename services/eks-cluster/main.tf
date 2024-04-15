cad754425f6dd4494275b980975a609290287fd0
ottagit <ottaosawa@gmail.com>
2024-04-15 23:34:58 +0300

    remove extra colon in sts::AssumeRole

--- services/eks-cluster/main.tf
+++ services/eks-cluster/main.tf
@@ -8,7 +8,7 @@ resource "aws_iam_role" "cluster" {
 data "aws_iam_policy_document" "cluster_assume_role" {
   statement {
     effect = "Allow"
-    actions = [ "sts::AssumeRole" ]
+    actions = [ "sts:AssumeRole" ]
     principals {
       type = "Service"
       identifiers = [ "eks.amazonaws.com" ]
