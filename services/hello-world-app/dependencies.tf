c1245e076f9369e94530d38fe64feb1c06b46de1
ottagit <ottaosawa@gmail.com>
2024-10-04 23:46:01 +0300

    refactor dependencies into own file

---
+++ services/hello-world-app/dependencies.tf
@@ -0,0 +1,21 @@
+data "aws_vpc" "default" {
+  default = true
+}
+
+data "aws_subnets" "default" {
+  filter {
+    name = "vpc-id"
+    values = [data.aws_vpc.default.id]
+  }
+}
+
+# Get the web server to read the outputs from the DB state file
+data "terraform_remote_state" "db" {
+  backend = "s3"
+
+  config = {
+    bucket = var.db_remote_state_bucket
+    key = var.db_remote_state_key
+    region = "us-east-1"
+  }
+}
--- services/hello-world-app/main.tf
+++ services/hello-world-app/main.tf
@@ -71,25 +71,3 @@ resource "aws_lb_listener_rule" "asg" {
     target_group_arn = aws_lb_target_group.asg.arn
   }
 }
-
-data "aws_vpc" "default" {
-  default = true
-}
-
-data "aws_subnets" "default" {
-  filter {
-    name = "vpc-id"
-    values = [data.aws_vpc.default.id]
-  }
-}
-
-# Get the web server to read the outputs from the DB state file
-data "terraform_remote_state" "db" {
-  backend = "s3"
-
-  config = {
-    bucket = var.db_remote_state_bucket
-    key = var.db_remote_state_key
-    region = "us-east-1"
-  }
-}
